e16439e7eab22e597aa28f05092c3ab3c15b5a48
lvitters <luvit023@gmail.com>
2022-01-23 00:13:15 +0100

    added emergence work in progress

---
+++ visuals/emergence/element.js
@@ -0,0 +1,146 @@
+//single element
+class Element {
+
+    constructor(size, xPos, yPos) {
+        
+        //position
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.pos = createVector(this.xPos, this.yPos);
+
+        //size & scale
+        this.size = size;
+        this.scale = 1;
+        this.scaleT = 0;
+
+        //rotation
+        this.rotation = 0;
+        this.roT = 0;
+        this.isRotating = false;
+        
+        //color
+        this.fillCol;
+        this.strokeCol;
+        this.hue;
+        this.sat;
+        this.bri;
+        this.hT = random(100); //hue
+        this.sT = random(100); //saturation
+        this.bT = random(100); //brightness
+
+        //stroke
+        this.strokeW;
+        this.strT;
+
+        //shape morphing
+        this.circle = [];
+        this.square = [];
+        this.morph = [];
+        this.state = 0;
+
+        this.initShapes();
+    }
+
+    //draw single element
+    draw() {
+        this.compute();
+
+        fill(this.fillCol);
+        strokeWeight(this.strokeW);
+        stroke(this.strokeCol);
+
+        push();
+            translate(this.pos.x, this.pos.y);
+            scale(this.scale);
+            if (this.isRotating) rotate(this.rotation);
+            this.drawShape();
+        pop();
+    }
+
+    compute() {
+        //color
+        this.hT += random(.001, .01);
+        this.sT += random(.001, .01);
+        this.bT += random(.001, .01);
+        this.hue = map(noise(this.hT), 0, 1, -60, 420);
+        this.sat = map(noise(this.sT), 0, 1, 50, 100);
+        this.bri = map(noise(this.bT), 0, 1, 50, 100);
+
+        //apply
+        this.fillCol = color(this.hue, this.sat, this.bri, fillAlpha);
+        this.strokeCol = color(this.hue, this.sat, strokeBrightness, strokeAlpha);
+
+        //stroke
+        this.strT = .01;
+        this.strokeW = map(noise(this.strT), 0, 1, -10, 40);
+
+        //scale
+        this.scaleT += random(.01);
+        this.scale = map(noise(this.scaleT), 0, 1, 0.2, 2);
+
+        //rotation
+        this.roT += random(.01);
+        this.rotation = map(noise(this.roT), 0, 1, 0, 10);
+        //if only circles are there, every x seconds, if the shapes aren't currently morphing, switch between rotating and not rotating
+        //(to hide the transition between rotation and no rotation)
+        if (this.state == 0 && frameCount % 300 == 0 && areMorphing == false) {
+            this.isRotating = !this.isRotating;
+            console.log("rotating: " + this.isRotating);
+        }
+    }
+
+    initShapes() {
+        //create a circle using vectors pointing from center
+        for (let angle = 0; angle < 360; angle += 9) {
+            //note we are not starting from 0 in order to match the
+            //path of a circle.
+            let v = p5.Vector.fromAngle(radians(angle - 135));
+            v.mult(this.size/2);
+            this.circle.push(v);
+            //let's fill out morph ArrayList with blank PVectors while we are at it
+            this.morph.push(createVector());
+        }
+
+        //a square is a bunch of vertices along straight lines
+        //top of square
+        for (let x = -this.size/2; x < this.size/2; x += this.size/2/5) {
+            this.square.push(createVector(x, -this.size/2));
+        }
+        //right side
+        for (let y = -this.size/2; y < this.size/2; y += this.size/2/5) {
+            this.square.push(createVector(this.size/2, y));
+        }
+        //bottom
+        for (let x = this.size/2; x > -this.size/2; x -= this.size/2/5) {
+            this.square.push(createVector(x, this.size/2));
+        }
+        //left side
+        for (let y = this.size/2; y > -this.size/2; y -= this.size/2/5) {
+            this.square.push(createVector(-this.size/2, y));
+        }
+    }
+
+    drawShape() {
+        //look at each vertex
+        for (let i = 0; i < this.circle.length; i++) {
+            let v1;
+            //are we lerping to the circle or square?
+            if (this.state == 0) {
+                v1 = this.circle[i];
+            } else if (this.state == 1) {
+                v1 = this.square[i];
+            }
+            //get the vertex we will draw
+            let v2 = this.morph[i];
+            //lerp to the target
+            v2.lerp(v1, 0.05);
+        }
+
+        //draw a polygon that makes up all the vertices
+        beginShape();
+            this.morph.forEach(v => {
+                vertex(v.x, v.y);
+            });
+        endShape(CLOSE);
+    }
+}
---
+++ visuals/emergence/emergence.js
@@ -0,0 +1,226 @@
+var elements = [];
+var elementSize;
+var gap;
+var elementsPerRow = 40;
+var areOverlapping = true;
+var areMorphing = false;
+var morphCounter = 0;
+var mode = 1;
+var lerpTime = 300;
+
+//background color lerping
+var bgColor = 360;
+var bgColorTarget;
+var bgLerpCount = 0;
+
+//fill color alpha lerping
+var fillAlpha;
+var fillAlphaTarget;
+var fillAlphaLerpCount;
+
+//stroke color brightness lerping
+var strokeBrightness;
+var strokeBrightnessTarget;
+var strokeBrightnessLerpCount;
+
+//stroke color alpha lerping
+var strokeAlpha;
+var strokeAlphaTarget;
+var strokeAlphaLerpCount;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    colorMode(HSB, 360, 100, 100, 100);
+    rectMode(CENTER);
+
+    //determine size of single element
+    elementSize = (windowWidth / elementsPerRow);
+
+    //determine size of gap
+    gap = - (elementSize / 5);
+
+    pushElements();
+}
+
+function draw() {
+    lerpBackground();
+    background(bgColor);
+    
+    frameRate(60);
+    
+    switchShapes();
+
+    //translate elements to middle of their position in grid
+    translate(elementSize/2, elementSize/2);
+    
+    //lerp stuff
+    lerpFillAlpha();
+    lerpStrokeBrightness();
+    lerpStrokeAlpha();
+
+    drawElements();
+}
+
+//push elements to list
+function pushElements() {
+    for (let i = -1; i < (windowWidth / elementSize) + 1; i++) {
+        for (let j = -1; j < (windowHeight / elementSize) + 1; j++) {
+            elements.push(new Element(elementSize, i * elementSize, j * elementSize));
+        }
+    }
+    //shuffle once at beginning in order to not get fish scale effect
+    shuffleArray(elements);
+}
+
+//draw all elements in list
+function drawElements() {
+    
+    //reset bool
+    areOverlapping = false;
+
+    //display elements and check for scale
+    for (let i = 0; i < elements.length; i++) {
+        let b = elements[i];
+        b.draw();
+        
+        //are any elements overlapping? (if the scale is over 1 and it is not only circles then they not overlapping)
+        if ((b.scale >= 1) || (b.state == 1)) areOverlapping = true;
+    }
+
+    //once every second, if no elements are overlapping, shuffle the elements so they will overlap differently 
+    //(because they are drawn on top of each other in the order of the array index)
+    if ((frameCount % 60 == 0) && (!areOverlapping)) {
+        shuffleArray(elements);
+        console.log("shuffled");
+    }
+}
+
+//switch the shapes in time interval
+function switchShapes() {
+    if (frameCount % 600 == 0) {
+        //set areMorphing to true
+        areMorphing = true;
+        for (let i = 0; i < elements.length; i++) {
+            let b = elements[i];
+            b.state++;
+            if (b.state > 1) {
+                b.state = 0;
+            }
+        }
+    }
+    //increment counter and reset after 3 seconds (180 frames)
+    morphCounter++;
+    if (morphCounter > 180) {
+        morphCounter = 0;
+        areMorphing = false;
+    }
+    //console.log(areMorphing);
+}
+
+//change background color in small steps 
+function lerpBackground() {
+    if (mode <= 2) {
+        bgColorTarget = 360;
+    } else {
+        bgColorTarget = 0;
+    }
+
+    if (bgColor != bgColorTarget && bgLerpCount < lerpTime) {
+        bgLerpCount++;
+        let amt = bgLerpCount/lerpTime;
+        var lerped = lerp(bgColor, bgColorTarget, amt);
+        bgColor = floor(lerped) + 1;
+    } else {
+        bgLerpCount = 0;
+        bgColor = bgColorTarget;
+    }
+}
+
+//change alpha(transparency) of fill color in small steps
+function lerpFillAlpha() {
+    if (mode <= 2) {
+        fillAlphaTarget = 0;
+    } else {
+        fillAlphaTarget = 100;
+    }
+
+    if (fillAlpha != fillAlphaTarget && fillAlphaLerpCount < lerpTime) {
+        fillAlphaLerpCount++;
+        let amt = fillAlphaLerpCount/lerpTime;
+        var lerped = lerp(fillAlpha, fillAlphaTarget, amt);
+        fillAlpha = floor(lerped);
+    } else {
+        fillAlphaLerpCount = 0;
+        fillAlpha = fillAlphaTarget;
+    }
+}
+
+//change brightness(how black or "colored-ness") of fill color in small steps
+function lerpStrokeBrightness() {
+    if (mode == 2) {
+        strokeBrightnessTarget = 100;
+    } else {
+        strokeBrightnessTarget = 0;
+    }
+
+    if (strokeBrightness != strokeBrightnessTarget && strokeBrightnessLerpCount < lerpTime) {
+        strokeBrightnessLerpCount++;
+        let amt = strokeBrightnessLerpCount/lerpTime;
+        var lerped = lerp(strokeBrightness, strokeBrightnessTarget, amt);
+        strokeBrightness = floor(lerped);
+    } else {
+        strokeBrightnessLerpCount = 0;
+        strokeBrightness = strokeBrightnessTarget;
+    }
+}
+
+//change brightness(how black or "colored-ness") of fill color in small steps
+function lerpStrokeAlpha() {
+    if (mode == 4) {
+        strokeAlphaTarget = 0;
+    } else {
+        strokeAlphaTarget = 100;
+    }
+
+    if (strokeAlpha != strokeAlphaTarget && strokeAlphaLerpCount < lerpTime) {
+        strokeAlphaLerpCount++;
+        let amt = strokeAlphaLerpCount/lerpTime;
+        var lerped = lerp(strokeAlpha, strokeAlphaTarget, amt);
+        strokeAlpha = floor(lerped);
+    } else {
+        strokeAlphaLerpCount = 0;
+        strokeAlpha = strokeAlphaTarget;
+    }
+}
+
+//check key presses
+function keyPressed() {
+    if (key == "1") {
+        for (let i = 0; i < elements.length; i++) {
+            let b = elements[i];
+            b.state = 0;
+        }
+    }    
+    if (key == "2") {
+        for (let i = 0; i < elements.length; i++) {
+            let b = elements[i];
+            b.state = 1;
+        }
+    }
+    //cycle through modes
+    if (keyCode == 32) {
+        if (mode < 4) mode += 1;
+        else mode = 1;
+        console.log(mode);
+    }
+}
+
+//randomize array in-place using Durstenfeld shuffle algorithm
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
