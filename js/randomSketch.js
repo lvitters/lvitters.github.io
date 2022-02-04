6b46767dc0a7369844de44682a4206f780fb5eb3
lvitters <luvit023@gmail.com>
2022-02-04 15:58:24 +0100

    implemented random sketch

---
+++ js/randomSketch.js
@@ -0,0 +1,15 @@
+function randomSketch() {
+    var urls = new Array();
+
+    urls[0] = "../../visuals/blob/index.html";
+    urls[1] = "../../visuals/doubleVortex/index.html";
+    urls[2] = "../../visuals/emergence/index.html";
+    urls[3] = "../../visuals/flowfield/index.html"
+    urls[4] = "../s../visuals/supernova/index.html";
+
+    var random = Math.floor(Math.random() * urls.length);
+
+    console.log(urls[random]);
+
+    window.location.href = urls[random];
+}
