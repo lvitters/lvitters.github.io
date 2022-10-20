4748143ce093b2faea728b0e3828b683c9a14e51
lvitters <luvit023@gmail.com>
2022-10-20 15:27:05 +0200

    removed doubleVortex from randomSketch()

--- js/randomSketch.js
+++ js/randomSketch.js
@@ -2,10 +2,10 @@ function randomSketch() {
     var urls = new Array();
 
     urls[0] = "../../visuals/blob/index.html";
-    urls[1] = "../../visuals/doubleVortex/index.html";
-    urls[2] = "../../visuals/emergence/index.html";
-    urls[3] = "../../visuals/carpet/index.html"
-    urls[4] = "../../visuals/supernova/index.html";
+    urls[1] = "../../visuals/emergence/index.html";
+    urls[2] = "../../visuals/carpet/index.html"
+    urls[3] = "../../visuals/supernova/index.html";
+    //urls[4] = "../../visuals/doubleVortex/index.html";
 
     var random = Math.floor(Math.random() * urls.length);
 
