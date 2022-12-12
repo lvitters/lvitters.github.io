f5b63d403bf83f38c4cfaf709161060b868138f2
lvitters <luvit023@gmail.com>
2022-12-12 14:12:40 +0100

    renamed emergence to untiled

--- js/randomSketch.js
+++ js/randomSketch.js
@@ -2,7 +2,7 @@ function randomSketch() {
     var urls = new Array();
 
     urls[0] = "../../visuals/blob/index.html";
-    urls[1] = "../../visuals/emergence/index.html";
+    urls[1] = "../../visuals/untiled/index.html";
     urls[2] = "../../visuals/carpet/index.html"
     urls[3] = "../../visuals/supernova/index.html";
     //urls[4] = "../../visuals/doubleVortex/index.html";
