d47d381bc5ab1d63971c735b583904c5de61c023
lvitters <luvit023@gmail.com>
2025-07-08 13:27:53 +0200

    add guard to prevent two sketches running at the same time on landing page

--- src/routes/works/+page.svelte
+++ src/routes/works/+page.svelte
@@ -1,29 +1,38 @@
 <script lang="ts">
-	import Nav from '$lib/components/Nav.svelte';
-
-	// add class to body for this page to remove scrollbar
-	$effect(() => {
-		document.documentElement.className += ' overflow-hidden h-screen w-screen';
-		document.body.className += ' overflow-hidden h-screen w-screen';
-
-		return () => {
-			document.documentElement.className = document.documentElement.className.replace(
-				' overflow-hidden h-screen w-screen',
-				''
-			);
-			document.body.className = document.body.className.replace(
-				' overflow-hidden h-screen w-screen',
-				''
-			);
-		};
-	});
 </script>
 
 <svelte:head>
 	<title>Lucca Vitters</title>
 </svelte:head>
 
-<main class="relative h-screen w-screen">
-	<!-- <Nav /> -->
-	<!-- sketch canvas -->
+<main class="relative flex h-screen w-screen overflow-hidden">
+	<!-- left 1/3 -->
+	<section class="h-full w-1/3">
+		<div>RAUSCHEN (2025)</div>
+		<div>Radio Angrezi Archive (2025)</div>
+		<div>RAYARRAY (2023)</div>
+		<div>feedback_cube (2022)</div>
+		<div>Ein Hauch von Tüll v2 (2022)</div>
+		<div>untiled (2022)</div>
+		<div>Ein Hauch von Tüll (2021)</div>
+		<div>available.rooms (2021)</div>
+		<div>blob (2020)</div>
+		<div>Break the Pattern (2019)</div>
+		<div>Image Blender (2017)</div>
+	</section>
+
+	<!-- right 2/3 -->
+	<section class="h-full w-2/3">
+		<div>RAUSCHEN (2025)</div>
+		<div>Radio Angrezi Archive (2025)</div>
+		<div>RAYARRAY (2023)</div>
+		<div>feedback_cube (2022)</div>
+		<div>Ein Hauch von Tüll v2 (2022)</div>
+		<div>untiled (2022)</div>
+		<div>Ein Hauch von Tüll (2021)</div>
+		<div>available.rooms (2021)</div>
+		<div>blob (2020)</div>
+		<div>Break the Pattern (2019)</div>
+		<div>Image Blender (2017)</div>
+	</section>
 </main>
--- static/sketches/blob.js
+++ static/sketches/blob.js
@@ -1,101 +1,105 @@
-let xT = 0.0;
-let blobs = [];
-let blobsLength = 20;
+// guard so no multiple sketches are running
+if (typeof sketchInitialized === 'undefined') {
+	var sketchInitialized = true;
 
-function setup() {
-	// assign sketch to website div
-	let container = document.getElementById('sketch-container');
-	let canvas = createCanvas(container.offsetWidth, container.offsetHeight);
-	canvas.parent('sketch-container');
-	canvas.id('canvas');
-	canvas.style('z-index', '1');
-	colorMode(RGB, 360, 360, 360, 360);
+	var xT = 0.0;
+	var blobs = [];
+	var blobsLength = 20;
 
-	pushBlobs();
-}
+	function setup() {
+		// assign sketch to proper container
+		var container = document.getElementById('sketch-container');
+		var canvas = createCanvas(container.offsetWidth, container.offsetHeight);
+		canvas.parent('sketch-container');
+		canvas.id('canvas');
+		canvas.style('z-index', '1');
+		colorMode(RGB, 360, 360, 360, 360);
 
-function draw() {
-	// reset background each frame
-	clear();
-	background(360, 0);
-
-	// move everything to center
-	translate(width / 2, height / 2);
-
-	// zoom out a bit
-	scale(0.8);
-
-	drawBlobs();
-}
-
-function pushBlobs() {
-	for (let i = 0; i < blobsLength; i++) {
-		blobs.push(new Blob(i));
+		pushBlobs();
 	}
-}
 
-function drawBlobs() {
-	for (let i = blobs.length - 1; i > 0; i--) {
-		let b = blobs[i];
-		b.draw();
+	function draw() {
+		// reset background each frame
+		clear();
+		background(360, 0);
+
+		// move everything to center
+		translate(width / 2, height / 2);
+
+		// zoom out a bit
+		scale(0.8);
+
+		drawBlobs();
 	}
-}
 
-function Blob(index) {
-	// blob's radius, gets bigger with higher index
-	let ra = index * 20;
-
-	// noise
-	let noiseMax = 0;
-	let zT = random(1);
-
-	// init noise values for colors
-	let rT = random(360);
-	let gT = random(360);
-	let bT = random(360);
-	//let alT = random(360);
-
-	this.draw = function () {
-		// noise
-		noiseMax = sin(zT);
-
-		// map color noise values to RGB(A)
-		let r = map(noise(rT), 0, 1, -30, 390);
-		let g = map(noise(gT), 0, 1, -30, 390);
-		let b = map(noise(bT), 0, 1, -30, 390);
-		//let al = map(noise(alT), 0, 1, -30, 390);
-
-		// apply colors
-		noStroke(0);
-		fill(r % 360, g % 360, b % 360);
-		//fill(r % 360, g % 360, b % 360, al % 360);
-
-		// draw blobs
-		beginShape();
-		for (let a = 0; a < TWO_PI; a += 0.08) {
-			let xT = map(cos(a), -1, 1, 0, noiseMax);
-			let yT = map(sin(a), -1, 1, 0, noiseMax);
-			let r = map(noise(xT, yT, zT), 0, 1, 0, ra);
-			let x = r * cos(a);
-			let y = r * sin(a);
-			vertex(x, y);
+	function pushBlobs() {
+		for (var i = 0; i < blobsLength; i++) {
+			blobs.push(new Blob(i));
 		}
-		endShape(CLOSE);
-
-		// increment noise values
-		zT += random(0.001, 0.005);
-
-		rT += random(0.001, 0.005);
-		gT += random(0.001, 0.005);
-		bT += random(0.001, 0.005);
-		//alT += random(0.001, 0.005);
-	};
-}
-
-function windowResized() {
-	let container = document.getElementById('sketch-container');
-	if (container) {
-		resizeCanvas(container.offsetWidth, container.offsetHeight);
 	}
-	setup();
+
+	function drawBlobs() {
+		for (var i = blobs.length - 1; i > 0; i--) {
+			var b = blobs[i];
+			b.draw();
+		}
+	}
+
+	function Blob(index) {
+		// blob's radius, gets bigger with higher index
+		var ra = index * 20;
+
+		// noise
+		var noiseMax = 0;
+		var zT = random(1);
+
+		// init noise values for colors
+		var rT = random(360);
+		var gT = random(360);
+		var bT = random(360);
+		//var alT = random(360);
+
+		this.draw = function () {
+			// noise
+			noiseMax = sin(zT);
+
+			// map color noise values to RGB(A)
+			var r = map(noise(rT), 0, 1, -30, 390);
+			var g = map(noise(gT), 0, 1, -30, 390);
+			var b = map(noise(bT), 0, 1, -30, 390);
+			//var al = map(noise(alT), 0, 1, -30, 390);
+
+			// apply colors
+			noStroke(0);
+			fill(r % 360, g % 360, b % 360);
+			//fill(r % 360, g % 360, b % 360, al % 360);
+
+			// draw blobs
+			beginShape();
+			for (var a = 0; a < TWO_PI; a += 0.08) {
+				var xT = map(cos(a), -1, 1, 0, noiseMax);
+				var yT = map(sin(a), -1, 1, 0, noiseMax);
+				var r = map(noise(xT, yT, zT), 0, 1, 0, ra);
+				var x = r * cos(a);
+				var y = r * sin(a);
+				vertex(x, y);
+			}
+			endShape(CLOSE);
+
+			// increment noise values
+			zT += random(0.001, 0.005);
+
+			rT += random(0.001, 0.005);
+			gT += random(0.001, 0.005);
+			bT += random(0.001, 0.005);
+			//alT += random(0.001, 0.005);
+		};
+	}
+
+	function windowResized() {
+		var container = document.getElementById('sketch-container');
+		if (container) {
+			resizeCanvas(container.offsetWidth, container.offsetHeight);
+		}
+	}
 }
