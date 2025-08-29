283a9834a310e95dd9a3ca21a7aeef186fb45304
lvitters <luvit023@gmail.com>
2025-08-29 19:04:01 +0200

    add enhancedImages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
-import devtoolsJson from 'vite-plugin-devtools-json';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
+	plugins: [tailwindcss(), enhancedImages(), sveltekit()],
 	build: {
 		chunkSizeWarningLimit: 1000,
 		rollupOptions: {
