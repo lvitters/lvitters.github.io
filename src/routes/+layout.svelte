74cd578097c623b5db52acb9e133418651fc3f0e
lvitters <luvit023@gmail.com>
2025-09-05 15:31:40 +0200

    remove force remount on breakpoint change

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,27 +4,6 @@
 
 	let { children } = $props();
 
-	// force remount on breakpoint changes by changing remountKey, in order for p5 sketches to remain after breakpoint change
-	let remountKey = $state(0);
-	let currentBreakpoint = $state(
-		browser ? (window.innerWidth >= 1024 ? 'desktop' : 'mobile') : 'desktop'
-	);
-	if (browser) {
-		const handleResize = () => {
-			const newBreakpoint = window.innerWidth >= 1024 ? 'desktop' : 'mobile';
-			if (newBreakpoint !== currentBreakpoint) {
-				currentBreakpoint = newBreakpoint;
-				// small delay to let any pending image requests complete
-				setTimeout(() => {
-					remountKey++; // force remount by changing remountKey
-				}, 100);
-			}
-		};
-
-		window.addEventListener('resize', handleResize);
-	}
 </script>
 
-{#key remountKey}
-	{@render children()}
-{/key}
+{@render children()}
